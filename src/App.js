73f1887eba7f59893236fc58e73025605091a9dd
Ashu Gautam <ashugautam009@gmail.com>
2023-12-06 00:15:22 +0530

    html and css work is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+import AddIcon from '@mui/icons-material/Add'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className='form'>
+        <Stack direction="row" spacing={2}>    
+          <TextField id="outlined-basic" label="name" variant='outlined'/>
+          <TextField id="outlined-basic" label="email" variant='outlined'/>
+          <Button variant='contained' color='success'>
+            <AddIcon/>
+          </Button>
+        </Stack>
+      </div>
+      
     </div>
   );
 }
